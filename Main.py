7d91d98300851a9749723dd6481dea399580f981
ericlon <ericlinjh@hotmail.com>
2021-06-26 13:05:18 -0400

    Add beginning of application

---
+++ Main.py
@@ -0,0 +1,26 @@
+import random
+
+def register():
+    newID = random.randint(1000000000000000, 9999999999999999)
+    print("Your new card number is now: " + str(newID))
+    newPW = input("Please enter a password: ")
+
+# introduction text
+print("░█████╗░███████╗██╗░░██╗  ██████╗░░█████╗░███╗░░██╗██╗░░██╗")
+print("██╔══██╗██╔════╝██║░██╔╝  ██╔══██╗██╔══██╗████╗░██║██║░██╔╝")
+print("██║░░╚═╝█████╗░░█████═╝░  ██████╦╝███████║██╔██╗██║█████═╝░")
+print("██║░░██╗██╔══╝░░██╔═██╗░  ██╔══██╗██╔══██║██║╚████║██╔═██╗░")
+print("╚█████╔╝███████╗██║░╚██╗  ██████╦╝██║░░██║██║░╚███║██║░╚██╗")
+print("░╚════╝░╚══════╝╚═╝░░╚═╝  ╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝")
+
+print("Welcome to CEK Bank!")
+
+# add customer check in here?
+print("Please select one of the following options:")
+print("1. Sign in")
+print("2. Register")
+choice = input()
+
+if (choice == "2"):
+    register()
+
