c06b4591139f1c7e98a0ebdfe809bdd9cf8d2215
ericlon <ericlinjh@hotmail.com>
2021-06-26 15:08:18 -0400

    Work on first function

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import random
+from Bank import *
 
 def register():
     newID = random.randint(1000000000000000, 9999999999999999)
@@ -23,4 +24,24 @@ choice = input()
 
 if (choice == "2"):
     register()
+else:
+    tempUser = input("Please enter your customer ID")
+    tempPassword = input("Please enter your password")
+    
 
+#
+# replace blank with customer name
+#
+print("Welcome " + "blank")
+print("Please select the function you would like to access:")
+print("1. Deposit")
+print("2. Withdraw")
+print("3. Display Name and ID")
+print("4. Display Balance")
+print("5. Change Account Details")
+print("6. Display Transaction History")
+choice = input()
+
+if (choice == "1"):
+    print("How much money would you like to deposit?")
+    Bank.deposit( )
