936b329064f0bbf60f363ced1ff638919d7ccec3
Carl Ansel Yu <carlayu@gmail.com>
2021-06-26 14:17:16 -0400

    Added instantiation in Main Class

--- Bank.py
+++ Bank.py
@@ -1,9 +1,11 @@
 class Bank: 
-    custID = 0; #customer ID 
-
-    def __init__(self): #initialize class
+    
+    def __init__(self): #initialize class with all the values from the spreadsheet
         pass 
 
     def createCustomer(self):
         customer[custID] = Customer() # Customer array, super wack pls fix. Add starting parameters
         custID += 1; #this counters the customer ID 
+    
+
+    
